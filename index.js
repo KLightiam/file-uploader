e460761ce3fa91c2fe155150dd55a1557960315a
Kingsley Asare <kingsleyasare20@yahoo.com>
2025-01-27 21:58:39 +0000

    Add start and build scripts to package.json

--- index.js
+++ index.js
@@ -8,18 +8,14 @@ const { expressSession } = require('./utils/expressSession');
 const { passportSession, passportAuthenticate, usePassport, serialize, deserialize } = require('./controller/passportController');
 const userRouter = require('./routes/userRouter');
 
-
 const app = express();
 
 app.set("views",path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
 app.use(express.static(path.join(__dirname,'public')));
-
 app.use(expressSession);
 app.use(passportSession);
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/", indexRouter);
 app.use("/sign-up", signUpRouter);
 app.use("/log-out",logoutRouter);
